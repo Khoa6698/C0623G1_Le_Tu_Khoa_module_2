1030b685d6e0af56d4d4e2b8b694d1f755478ff8
Khoa6698 <lekhoa661998@gmail.com>
2023-08-03 10:32:52 +0700

    da sua

--- ss1_introdution_to_java/bai_tap/MoneyCheck.java
+++ ss1_introdution_to_java/bai_tap/MoneyCheck.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class MoneyCheck {
     public static void main(String[] args) {
-        double vnd = 23000;
+        double rate = 23000;
         double usd;
         Scanner scanner = new Scanner(System.in);
         System.out.print("What money your check USA>VND: ");
         usd = scanner.nextDouble();
-        double check = usd * 23000;
-        System.out.println("VND money: "+check);
+        double vnd = usd * 23000;
+        System.out.println("VND money: "+vnd);
     }
 }
