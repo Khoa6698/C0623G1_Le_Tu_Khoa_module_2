2753da674a0ae14acd7da06696a8b1b0916a99ae
Khoa6698 <lekhoa661998@gmail.com>
2023-08-24 06:26:17 +0700

    ss17_binary_file_serialization

--- ss17_binary_file_serialization/bai_tap/copy_file_binary/CopyFileBinary.java
+++ ss17_binary_file_serialization/bai_tap/copy_file_binary/CopyFileBinary.java
@@ -1,51 +1,47 @@
 package ss16_binary_file_serialization.bai_tap.copy_file_binary;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 public class CopyFileBinary {
-    private static void copyFile(String sourceFile,String targetFile) throws IOException {
+    private static void copyFile(String sourceFile, String targetFile) throws IOException {
         FileInputStream fileInputStream = new FileInputStream(sourceFile);
         FileOutputStream fileOutputStream = new FileOutputStream(targetFile);
         byte[] bytes = new byte[1024];
-        int bytesRead;
-        while ((bytesRead = fileInputStream.read())!=-1){
-            fileOutputStream.write(bytes,0,bytesRead);
+        System.out.println("Total bytes that can be read : " + fileInputStream.available());
+        while (fileInputStream.read(bytes) != -1) {
+            System.out.println("Content of source file: " + new String(bytes, StandardCharsets.UTF_8));
         }
+        fileOutputStream.write(bytes, 0, bytes.length);
+        fileOutputStream.flush();
     }
 
-    private static boolean checkSourceFile(String sourceFilePath){
+    private static boolean checkSourceFile(String sourceFilePath) {
         File sourceFile = new File(sourceFilePath);
-        if(!sourceFile.exists()){
+        if (!sourceFile.exists()) {
             System.out.println("Loi: tep dich khong ton tai");
             return false;
         }
         return true;
     }
 
-    private static boolean checkTargetFile(String targetFilePath){
+    private static boolean checkTargetFile(String targetFilePath) {
         File targetFile = new File(targetFilePath);
-        if(targetFile.exists()){
+        if (targetFile.exists()) {
             System.out.println("Tep dich da ton tai");
             return true;
         }
         return false;
     }
 
-    private static void displayFile(String targetFilePath){
-        File targetFile = new File(targetFilePath);
-        long fileSize = targetFile.length();
-        System.out.println("So file trong tep dich: "+fileSize);
-    }
-
     public static void main(String[] args) {
         String sourceFilePath = "C:\\Users\\admin\\IdeaProjects\\module2\\src\\ss16_binary_file_serialization\\bai_tap\\copy_file_binary\\source.dat";
         String targetFilePath = "C:\\Users\\admin\\IdeaProjects\\module2\\src\\ss16_binary_file_serialization\\bai_tap\\copy_file_binary\\target.dat";
         try {
-            if(checkSourceFile(sourceFilePath)&&!checkTargetFile(targetFilePath)){
-                copyFile(sourceFilePath,targetFilePath);
-                displayFile(targetFilePath);
+            if (checkSourceFile(sourceFilePath) && !checkTargetFile(targetFilePath)) {
+                copyFile(sourceFilePath, targetFilePath);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Loi: Khong the sao chep tep.");
         }
     }
